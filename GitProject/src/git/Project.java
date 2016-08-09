0f78d37fd65ffff4a564effcd9e3685b9856add2
chaitrashreebk <chaitrashree.bk@gmail.com>
2016-08-09 17:05:38 +0530

    Final commit

--- GitProject/src/git/Project.java
+++ GitProject/src/git/Project.java
@@ -9,9 +9,6 @@ public class Project {
 		System.out.println("Hello");
 		System.out.println("Hello1");
 		System.out.println("Hello2");
-		System.out.println("Hello3");
-		System.out.println("Hello4");
-		
 		
 		
 
